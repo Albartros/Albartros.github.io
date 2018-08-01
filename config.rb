a50089986f378c1809f46cc2811de699c2ddacc6
Jim <jim.zoladek@outlook.com>
2018-08-01 08:45:26 +0200

    Retried

---
+++ config.rb
@@ -0,0 +1,10 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'sass'
+images_dir = 'img'
+relative_assets = true
+output_style = :compressed
+
+require "animate"
+require "susy"
